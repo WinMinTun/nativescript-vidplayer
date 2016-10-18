663c1d0a7f48f89d86ca6bee00604c4f61abf07a
Win Min Tun <winmintun@techfun-myanmar.com>
2016-10-18 12:59:22 +0630

    unhide nav bar

--- vidplayer.android.ts
+++ vidplayer.android.ts
@@ -195,9 +195,9 @@ export class Vidplayer extends common.VidPlayer {
                 var decorView = window.getDecorView();
                 // Hide the status bar, navigation bar, and enter immersive sticky.
                 let uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    //| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                     | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                    //| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
                     | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
                     | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
                 decorView.setSystemUiVisibility(uiOptions);
